c425a385d5e07fb004b001ad7bbd1f613b163ecd
unknown <bugialfaridzi@gmail.com>
2024-07-25 14:50:16 +0700

    nextjs-dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import style from '@/app/ui/home.module.css'
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -20,10 +21,10 @@ export default function Page() {
             I have experience in building web applications using React, Node.js and CodeIgniter. I'm also familiar with AWS and Docker.
         </p>
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Login</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Next</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
@@ -47,22 +48,3 @@ export default function Page() {
   );
 }
 
-// import AcmeLogo from '@/app/ui/acme-logo';
-// import { ArrowRightIcon } from '@heroicons/react/24/outline';
-// import Link from 'next/link';
-// import style from '@/app/ui/home.module.css'
- 
-// export default function Page() 
-// {
-//   return (
-//     // These are Tailwind classes:
-//     <main className="flex min-h-screen flex-col p-6">
-//       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-//         <div classname={style.shape} />
-//     // ...
-//   )
-// }
-
-// <div
-//   className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
-// />
