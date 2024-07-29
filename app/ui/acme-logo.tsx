97c3320943e75539576d2f8f05850c03ddac20e6
unknown <bugialfaridzi@gmail.com>
2024-07-29 09:54:25 +0700

    'nextjs-dashboard'

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -7,7 +7,7 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-10 w-12 rotate-[15deg]" />
-      <p className="text-[40px]">Bugi Alfaridi</p>
+      <p className="text-[30px]">Bugi Alfaridi</p>
     </div>
   );
 }
